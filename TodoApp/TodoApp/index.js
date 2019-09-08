801a06696d8c03fec74e488cef4581422bcd9440
Nhan Tran <nhantran@STS-NHANTRAN.lan>
2019-09-08 13:23:09 +0700

    update todo list

--- TodoApp/TodoApp/index.js
+++ TodoApp/TodoApp/index.js
@@ -14,6 +14,8 @@ export default class TodoApp extends React.PureComponent {
   state = {
     todoList: [],
     val: '',
+    searchText: '',
+    searchResults: [],
   };
 
   addItem = () => {
@@ -30,6 +32,18 @@ export default class TodoApp extends React.PureComponent {
     this.setState({
       todoList: newTodoList,
       val: '',
+    });
+  };
+
+  searchItems = () => {
+    const {todoList, searchText} = this.state;
+    const results = _.filter(todoList, item =>
+      _.includes(item.value, searchText),
+    );
+
+    this.setState({
+      searchResults: results,
+      searchText: '',
     });
   };
 
@@ -65,11 +79,34 @@ export default class TodoApp extends React.PureComponent {
     });
   };
 
+  handleChangeSearchText = text => {
+    this.setState({
+      searchText: text,
+    });
+  };
+
   render() {
-    const {val, todoList} = this.state;
+    const {val, todoList, searchText, searchResults} = this.state;
     const sortedTodoList = _.sortBy(todoList, 'status');
+    const sortedSearchResults = _.sortBy(searchResults, 'status');
     return (
       <View style={styles.todoApp}>
+        <View style={styles.addingForm}>
+          <TextInput
+            style={styles.input}
+            value={searchText}
+            onChangeText={this.handleChangeSearchText}
+          />
+          <TouchableHighlight style={styles.button} onPress={this.searchItems}>
+            <Text style={styles.buttonText}>Search</Text>
+          </TouchableHighlight>
+        </View>
+        <TodoList
+          onDelete={this.handleDelete}
+          onComplete={this.handleComplete}
+          todoList={sortedSearchResults}
+        />
+
         <View style={styles.addingForm}>
           <TextInput
             style={styles.input}
